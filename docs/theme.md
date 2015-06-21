262bd4e4c098e85d859d54371c16f8741766a883
Richard Zetterberg <richard.zetterberg@googlemail.com>
2015-06-21 23:56:21 +0200

    Adds start for more lua functions

--- docs/theme.md
+++ docs/theme.md
@@ -6,10 +6,9 @@ routing, connecting to the database, marshalling data etc.
 
 ## Theme contents
 
-A theme should consist of 2 Lua files to function:
+A theme should consist of 1 Lua file to function:
 
  - page.lua
- - page_404.lua
 
 ### page.lua
 
@@ -18,13 +17,6 @@ database. To render the view this file can use the
 [get_current_page](#get_current_page()) API function to retrieve the current
 page. 
 
-If the page is not found, the `page_404.lua` is ran instead.
-
-### page_404.lua
-
-This file is responsible for rendering a view when the requested page does not
-exist.
-
 ## API reference
 
 ### get_current_page()
@@ -32,7 +24,8 @@ exist.
 This function retrieves the page that the user is currently visiting. The core
 handles the routing and selects the current page from the database.
 
-A `table` that represents the current page is returned. 
+A `table` that represents the current page is returned, or `nil` if no page was
+found.
 
 ### get_navigation()
 
--- src/Handler/Public/Page.hs
+++ src/Handler/Public/Page.hs
@@ -1,5 +1,6 @@
 module Handler.Public.Page where
 
+import qualified Data.Text as T
 import           Foreign.C.Types (CInt)
 import           Import
 import qualified Scripting.Lua as Lua
@@ -13,24 +14,26 @@ getPageViewR permalink = do
     userM           <- maybeAuthId
 
     if (textPagePublic page) || (isJust userM)
-       then go
+       then go page
        else notFound
   where
-    go = do
-        res <- liftIO $ runPageTheme
+    go page = do
+        res <- liftIO $ runPageTheme page
 
         return $ case res of
             Nothing -> toHtml ("Error in Lua!" :: String)
             Just d  -> toHtml d
 
-runPageTheme :: IO (Maybe String)
-runPageTheme = do
+runPageTheme :: TextPage -> IO (Maybe String)
+runPageTheme page = do
     outputRef <- newIORef ""
     lstate    <- Lua.newstate
 
     Lua.openlibs lstate
     Lua.registerrawhsfunction lstate "print"
-        (print_to_core outputRef)
+        (collectPrint outputRef)
+    Lua.registerrawhsfunction lstate "get_current_page"
+        (getCurrentPage page)
 
     res <- Lua.loadfile lstate "test/lua/output.lua" >>= go lstate
 
@@ -43,12 +46,30 @@ runPageTheme = do
     go lstate 0 = Lua.pcall lstate 0 Lua.multret 0
     go _ _      = return 1
 
+--------------------------------------------------------------------------------
+-- * Lua functions
+
 -- | Used to redefine Luas global print function to save output into a buffer
 -- instead, so that the data can be accessed later via an `IORef`.
-print_to_core :: IORef String -> LuaState -> IO CInt
-print_to_core outputRef lstate = do
+collectPrint :: IORef String -> LuaState -> IO CInt
+collectPrint outputRef lstate = do
     luaData <- Lua.tostring lstate 1
 
     modifyIORef' outputRef (++ luaData)
 
     return 0
+
+getCurrentPage :: TextPage -> LuaState -> IO CInt
+getCurrentPage TextPage{..} lstate = do
+    Lua.createtable lstate 0 3
+
+    Lua.pushstring lstate (T.unpack textPageName)
+    Lua.setfield lstate (-2) "name"
+
+    Lua.pushstring lstate (T.unpack textPagePermalink)
+    Lua.setfield lstate (-2) "permalink"
+
+    Lua.pushstring lstate "Not implemented yet"
+    Lua.setfield lstate (-2) "body"
+
+    return 1
