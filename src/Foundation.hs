955547e3c49179c9b0d5c12a9afea23e08e9797b
Richard Zetterberg <richard.zetterberg@googlemail.com>
2015-07-12 16:06:37 +0200

    Adds redirect to admin after password set

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -194,7 +194,7 @@ instance YesodAuthEmail App where
 
     getEmail uid = runDB (get uid) >>= return . fmap userEmail
 
-    afterPasswordRoute _ = HomeR
+    afterPasswordRoute _ = AdminR
 
 -- This instance is required to use forms. You can modify renderMessage to
 -- achieve customized and internationalized form validation messages.
